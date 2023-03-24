88e49c6c020795ff6703ce4ee33c74d08ff840b3
Noha1029 <noha.elhouss@gmail.com>
2023-03-24 16:22:40 -0700

    yarbi

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
 include "main.h"
 /**
-* * main - tests function that prints if integer is positive or negative
-* * main - tests function that prints if integer is positive or negative
-* * i - character to integer
-* * i - character to integer
-* * Trying to figurout this
-* * Return: 0
-* */
+* main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative
+* i - character to integer
+* i - character to integer
+* Trying to figurout this
+* Return: 0
+*/
 int main(void)
 {
 int i;
