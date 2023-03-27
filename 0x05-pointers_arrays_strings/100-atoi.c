f650359e23da2a40798f8c77276c7c5913e5c32c
Noha1029 <noha.elhouss@gmail.com>
2023-03-27 12:40:25 -0700

    yarbi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,16 +13,16 @@ int brk = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == '-')
-sig = sig * -1;
+sig = sig *-1;
 if (s[i] >= '0' && s[i] <= '9')
 {
-res = res * 10;
+res = res *10;
 res -= (s[i] - '0');
 brk = 1;
 }
 else if (brk == 1)
 break;
 }
-res = sig * res;
+res = sig *res;
 return (res);
 }
