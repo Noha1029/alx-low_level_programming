940730024e2f4b9910006741f208186c0d73ffa4
Noha1029 <noha.elhouss@gmail.com>
2023-04-19 14:08:06 -0700

    yarbi

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "functions_pointers.h"
+/**
+* print_na - prints a na
+* @na :na's main
+* @f : void function
+*/
+void print_na(char *name, void ( *f)(char *))
+{
+if (na && f)
+{
+f(na);
+}
+}
